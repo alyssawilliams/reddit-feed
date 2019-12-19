8c14f804db88071889eaa26be3855f5571b608b9
Alyssa Williams <awilliamsdev@gmail.com>
2019-12-18 20:37:21 -0500

    Setting up components

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header.js';
+import Feed from './Feed.js';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Feed />
+    </div>
+  );
+}
+
+export default App;
