006038761be23b23380b5f08b0ae319bea0f1df0
Alyssa Williams <awilliamsdev@gmail.com>
2019-12-22 16:04:16 -0500

    Updates

---
+++ precache-manifest.a865e4528b7c59bab159be1b40d144f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a90e66e1b5b5a1815453bfcd8d16b36",
+    "url": "/reddit-feed/index.html"
+  },
+  {
+    "revision": "c1e40e9524fb96a3766a",
+    "url": "/reddit-feed/static/css/main.da0ef36a.chunk.css"
+  },
+  {
+    "revision": "538784f09925d323267f",
+    "url": "/reddit-feed/static/js/2.00d03c3d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reddit-feed/static/js/2.00d03c3d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1e40e9524fb96a3766a",
+    "url": "/reddit-feed/static/js/main.fb60718c.chunk.js"
+  },
+  {
+    "revision": "958c7c254b51cdb0b326",
+    "url": "/reddit-feed/static/js/runtime-main.3761239f.js"
+  }
+]);
